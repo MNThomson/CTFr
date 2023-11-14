e80b106d6e808f14661431d1c5a7fd22ae899990
Max Thomson <git@mnt.dev>
2023-11-13 21:29:12 -0800

    [O11Y] Add basic tracing setup

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # CTFr
 
+[<img alt="github" src="https://img.shields.io/badge/github-MNThomson/CTFr-bc3f48?style=for-the-badge&labelColor=555555&logo=github" height="20">](https://github.com/MNThomson/CTFr)
+[<img alt="crates.io" src="https://img.shields.io/crates/v/CTFr.svg?style=for-the-badge&color=fc8d62&logo=rust" height="20">](https://crates.io/crates/CTFr)
+[<img alt="build status" src="https://img.shields.io/github/actions/workflow/status/MNThomson/CTFr/ci.yml?branch=master&style=for-the-badge" height="20">](https://github.com/MNThomson/CTFr/actions?query=branch%3Amaster)
+
 _Optimized `CTFd` written with Rust & HTMX_
+
+#### License
+
+<sup>
+Licensed under <a href="LICENSE">GPLv3</a>
+</sup>
+<br>
+<sub>
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in this crate by you shall be licensed as above, without any additional terms or conditions
+</sub>
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use axum::{response::Html, routing::get, Router};
 use std::net::SocketAddr;
+use tracing::{debug, info};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 const BANNER: &str = r#"
  ██████╗████████╗███████╗
@@ -13,11 +15,21 @@ const BANNER: &str = r#"
 
 #[tokio::main]
 async fn main() {
+    println!("{}", BANNER);
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "ctfr=debug,axum::rejection=trace".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .try_init()
+        .expect("Tracing to start");
+
     let app = Router::new().route("/", get(handler));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 4321));
-    println!("{}", BANNER);
-    println!("listening on {}", addr);
+    debug!("listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
